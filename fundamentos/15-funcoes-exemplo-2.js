4f6342451b537219c60905ea00c7a83e2e4ea61d
MacedoCoding <macedo@tuta.io>
2021-09-20 21:41:10 -0300

    Update 15-funcoes-exemplo-2.js

---
+++ fundamentos/15-funcoes-exemplo-2.js
@@ -0,0 +1,5 @@
+let areaRetangulo = (lado1, lado2) => {return (lado1 * lado2).toFixed(2)}
+let areaTriangulo = (base, h) => {return ((base * h) / 2).toFixed(2)}
+let areaTrapezio = (B, b, h) => {return ((B + b) * h / 2).toFixed(2)}
+let areaCirculo = raio => {return (Math.PI * Math.pow(raio, 2)).toFixed(2)}
+let raiz1Grau = (a, b = 0) => {return (- 1 * b / a).toFixed(2)}
