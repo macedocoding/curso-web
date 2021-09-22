9d3229d047285e022a5544dc93c44672aa3f4e84
MacedoCoding <macedo@tuta.io>
2021-09-22 20:24:22 -0300

    Update 24-operador-destructuring-array.js

--- fundamentos/24-operador-destructuring-array.js
+++ fundamentos/24-operador-destructuring-array.js
@@ -1,2 +1,5 @@
-const [a] = [10];
+const [a] = [10]; // Extraindo o valor de um array pelo operado destructuring
 console.log(a);
+
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]; // 
+console.log(n1, n3, n5, n6);
